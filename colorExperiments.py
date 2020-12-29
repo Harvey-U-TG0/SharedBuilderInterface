099a63b0e605159a16764bbe3c04df646c12ceda
“[Harvey <[harv.upton@icloud.com]>
2020-12-29 12:16:50 +0000

    Brick Identification Using HSV ranges

--- colorExperiments.py
+++ colorExperiments.py
@@ -76,9 +76,10 @@ print("Blue calibration is {}".format(blueCalib))
 print("Yellow calibration is {}".format(yellowCalib))
 print("Red calibration is {}".format(redCalib))
 
-# Setting ranges
+# Setting ranges, The + or - range from the detected mean from the calibration blocks
 greenHueRange = 15
 greenSatRange = 60
+greenBriRange = 40
 
 greenHueMin = greenCalib[0]-greenHueRange
 greenHueMax = greenCalib[0]+greenHueRange
@@ -90,11 +91,82 @@ greenSatMax = greenCalib[1]+greenSatRange
 greenSatMask = cv2.inRange(hsvImg, np.array([0,greenSatMin,0]), np.array([180,greenSatMax,255]))
 cv2.imwrite('GreenMasks/greenSatMask.png', greenSatMask)
 
-greenBricks = np.zeros([studCount,studCount])
-print (greenBricks)
+greenBriMin = greenCalib[2]-greenBriRange
+greenBriMax = greenCalib[2]+greenBriRange
+greenBriMask = cv2.inRange(hsvImg, np.array([0,0,greenBriMin]), np.array([180,255,greenBriMax]))
+cv2.imwrite('GreenMasks/greenBriMask.png', greenBriMask)
 
+greenBricks = np.zeros([studCount,studCount])
 
 #Add nested loops to Bitmask Add two requirements
+for rowIndex in range(greenBricks.shape[0]):
+    for colIndex in range(greenBricks.shape [1]):
+        if (greenHueMask[rowIndex,colIndex] == 255) and (greenSatMask[rowIndex,colIndex] == 255) and (greenBriMask[rowIndex,colIndex] == 255):
+            greenBricks[rowIndex,colIndex] = 255
+
+cv2.imwrite('GreenMasks/GreenBricks.png', greenBricks)
+
+
+blueHueRange = 10
+blueSatRange = 80
+blueBriRange = 25
+
+blueHueMin = blueCalib[0]-blueHueRange
+blueHueMax = blueCalib[0]+blueHueRange
+blueHueMask = cv2.inRange(hsvImg, np.array([blueHueMin,0,0]), np.array([blueHueMax,255,255]))
+cv2.imwrite('BlueMasks/blueHueMask.png', blueHueMask)
+
+blueSatMin = blueCalib[1]-blueSatRange
+blueSatMax = blueCalib[1]+blueSatRange
+blueSatMask = cv2.inRange(hsvImg, np.array([0,blueSatMin,0]), np.array([180,blueSatMax,255]))
+cv2.imwrite('BlueMasks/blueSatMask.png', blueSatMask)
+
+blueBriMin = blueCalib[2]-blueBriRange
+blueBriMax = blueCalib[2]+blueBriRange
+blueBriMask = cv2.inRange(hsvImg, np.array([0,0,blueBriMin]), np.array([180,255,blueBriMax]))
+cv2.imwrite('BlueMasks/blueBriMask.png', blueBriMask)
+
+blueBricks = np.zeros([studCount,studCount])
+
+#Add nested loops to Bitmask Add two requirements
+for rowIndex in range(blueBricks.shape[0]):
+    for colIndex in range(blueBricks.shape [1]):
+        if (blueHueMask[rowIndex,colIndex] == 255) and (blueSatMask[rowIndex,colIndex] == 255) and (blueBriMask[rowIndex,colIndex] == 255):
+            blueBricks[rowIndex,colIndex] = 255
+
+cv2.imwrite('BlueMasks/blueBricks.png', blueBricks)
+
+
+yellowHueRange = 3
+yellowSatRange = 115
+yellowBriRange = 30
+
+yellowHueMin = yellowCalib[0]-yellowHueRange
+yellowHueMax = yellowCalib[0]+yellowHueRange
+yellowHueMask = cv2.inRange(hsvImg, np.array([yellowHueMin,0,0]), np.array([yellowHueMax,255,255]))
+cv2.imwrite('YellowMasks/yellowHueMask.png', yellowHueMask)
+
+yellowSatMin = yellowCalib[1]-yellowSatRange
+yellowSatMax = yellowCalib[1]+yellowSatRange
+yellowSatMask = cv2.inRange(hsvImg, np.array([0,yellowSatMin,0]), np.array([180,yellowSatMax,255]))
+cv2.imwrite('YellowMasks/yellowSatMask.png', yellowSatMask)
+
+yellowBriMin = yellowCalib[2]-yellowBriRange
+yellowBriMax = yellowCalib[2]+yellowBriRange
+yellowBriMask = cv2.inRange(hsvImg, np.array([0,0,yellowBriMin]), np.array([180,255,yellowBriMax]))
+cv2.imwrite('YellowMasks/yellowBriMask.png', yellowBriMask)
+
+yellowBricks = np.zeros([studCount,studCount])
+
+#Add nested loops to Bitmask Add two requirements
+for rowIndex in range(yellowBricks.shape[0]):
+    for colIndex in range(yellowBricks.shape [1]):
+        if (yellowHueMask[rowIndex,colIndex] == 255) and (yellowSatMask[rowIndex,colIndex] == 255) and (yellowBriMask[rowIndex,colIndex] == 255):
+            yellowBricks[rowIndex,colIndex] = 255
+
+cv2.imwrite('YellowMasks/yellowBricks.png', yellowBricks)
+
+
 
 
 # Visulisation
