20f7653b34f0b4f8da09422d4b7064515b5c6f1d
“[Harvey <[harv.upton@icloud.com]>
2021-01-05 14:51:20 +0000

    Sending test to the server from raspberry pi

---
+++ InterfacePrograms/testData.py
@@ -0,0 +1,22 @@
+class InterfaceTestData:
+
+    arrangementA = {
+        "bricks": [
+            {
+                "type": "2x2",
+                "position": [
+                    1,
+                    2
+                ],
+                "colour": "green"
+            },
+            {
+                "type": "1x1",
+                "position": [
+                    0,
+                    0
+                ],
+                "colour": "red"
+            }
+        ],
+    }
